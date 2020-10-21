f72bb65f4d32f611b2ef3756e02f37ee461de8e5
Timcosse <32547831+Timcosse@users.noreply.github.com>
2020-10-21 09:34:49 +0200

    Update Extreme_value_GPDC.R
    Positive distances do not work with assumption of Theorem 3

--- Extreme_value_GPDC.R
+++ Extreme_value_GPDC.R
@@ -9,7 +9,7 @@ require("evd")
 # From E. Vignotto
 
 # Set Working Directory
-wd <- "/Users/Timot/OneDrive/Bureau//mémoire"
+wd <- "/Users/Timot/OneDrive/Bureau//mÃ©moire"
 setwd(wd)
 
 
@@ -28,27 +28,6 @@ processed_set <- remove_class(data = mydata, class_column = 17, class_excluded =
 GPDC <- function(trainingdata, data_to_be_tested, max_neighbours, threshold_value) 
 {
   
-  res <- numeric()
-  distances_base <- numeric()
-  distances_test <- numeric()
-
-  
-  #Testing a new set of observations
-  distances_test <- numeric()
-  
-  #init objects to store results
-  #vector of distances
-  distances_test <- numeric()
-  #dataframe of results
-  m <- matrix(0, ncol = length(q_values), nrow = nrow(data_to_be_tested))
-  test_results <- data.frame(m)
-  
-  #Results dataframe
-  for (i in 1:length(q_values)) {
-    test_results[,i] <- quant_num[i]
-  }
-  
-  
   #Distances in base set
   distances <- nn2(trainingdata,trainingdata,k=max_neighbours)
   distances <- distances$nn.dist
@@ -56,32 +35,13 @@ GPDC <- function(trainingdata, data_to_be_tested, max_neighbours, threshold_valu
   #remove distances with same obs (itself)
   distances <- subset( distances, select = -1 )
   
-  #Use thresold the k smallest distances values to estimate Epsi 
-  threshold <- distances[,1]
-  distances2 <- distances[,-1]
-  
-  
-  R <- (distances2)/threshold
-  
-  #The mixture features
-  shape <- apply(R, 1, function(x) mean(log(x[x != 0])))
+
   
   
 
-  
-  #Vignotto version
-  #ball <- apply(cbind(shape,threshold),1,function(x)
-  #  -evd::qgpd(q,x[2],x[1]*x[2],x[1]))
-  #pshape <- p*shape
-  
   
 }
 
-
-
-#test3 <- EVMachine()
-#test_3 <- EVMachine(trainingdata = processed_set$Without_class, data_to_be_tested = processed_set$Selected_class, 
-# boot_nb=10, q_values=c(0.90, 0.925, 0.95, 0.975, 0.98, 0.99)
 
 # )
 #max_neighbours <- 4
